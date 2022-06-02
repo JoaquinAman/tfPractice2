35f808784e077f8d9718c4de333690bd0a71f8f4
JoaquinAman <55667385+JoaquinAman@users.noreply.github.com>
2022-06-02 00:09:34 -0300

    deleting keys credentials

--- keys.tf
+++ keys.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "deployer" {
-  key_name   = "JoaquinAman-KP"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQChkbhbk7WwhZO1Frk9uNGU1Lr/MSLevhwlMLSdSegXzs2I3Jw5EV4lb0rHbbN2lgnvl3Ax/ubYyayJ1s+Fk4Mxsws0EA0DLnW8Qwn6I8sKED2u/N5fzsj1xnb6zVvKjxk9V0ntarX2VolPrT+76oYV1+tfSxVnYuBX48u/kS/2R4HcuO5EyDkhenvg3RW3dYClXtbUavcA2pnhGy4rsxrtKilV0VhluCKbTCUe/igQBNX+uZ0IC/7b9SSot5vH86jFfW298NjxMWdJaCG0IMH4XbBVrQ1IswQDGuSb573JBhQMPU2rKk6YlYujMMO/D2Gxz2N1ea7o4EsA7Sm5gD/B"
+  key_name   = "..."
+  public_key = "..."
 }
